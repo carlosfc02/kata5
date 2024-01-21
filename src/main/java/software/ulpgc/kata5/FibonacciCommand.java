5a331a1fce70b040904c7cd92718afd075e73cca
carlosfc02 <carlosfalconcastellano@gmail.com>
2024-01-21 12:09:50 +0000

    FibonacciCommand created and done, the same as FactorialCommand changing the method factorialOf() by the fibonacciOf()

--- src/main/java/software/ulpgc/kata5/FibonacciCommand.java
+++ src/main/java/software/ulpgc/kata5/FibonacciCommand.java
@@ -2,7 +2,8 @@ package software.ulpgc.kata5;
 
 import java.util.stream.IntStream;
 
-public class FactorialCommand implements  Command{
+public class FibonacciCommand implements Command {
+
     @Override
     public Output execute(Input input) {
         try{
@@ -36,13 +37,15 @@ public class FactorialCommand implements  Command{
 
             @Override
             public String result() {
-                return String.valueOf(factorialOf(number));
+                return String.valueOf(fibonacciOf(number));
             }
         };
     }
 
-    private static int factorialOf(int number) {
-        return IntStream.range(2,number).reduce(1, (a, i)->a*i);
+    private static int fibonacciOf(int number) {
+        return IntStream.range(0,number)
+                .reduce((a,b) -> b == 0 ? 0 : (b == 1 ? 1 : a + b))
+                .orElse(0);
     }
 
     private Output outOfBoundOutput() {
